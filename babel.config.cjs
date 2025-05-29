8abc905a92119342b616ddac8188792480cef894
ewurasi nyarkoa <anewurasi@gmail.com>
2025-05-29 07:53:55 +0000

    checking if husky has been removed

---
+++ babel.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['> 1%', 'last 2 versions', 'not dead'],
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+};
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ interface Tool {
 // If your JSON is an array of tools
 const main = document.getElementById('tool-container');
 if (main) {
-  (tools as Tool[]).forEach(tool => {
+  (tools as Tool[]).forEach((tool) => {
     const div = document.createElement('div');
     div.className = 'tool';
     div.innerHTML = `
